70afee797ba89bc3c64e66679b9d6008ab9833a9
Catherine Sawyer <crsawyer314@gmail.com>
2022-10-12 14:51:47 -0700

    try 2 deploy to gh

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 
-import App from './App';
+import Sidebar from './component/Sidebar';
+import Home from './component/Home';
+import JsAbout from './component/JsAbout';
+import JsSyllabus from './component/JsSyllabus';
+import Resumes from './component/Resumes';
+import Hobbies from './component/Hobbies';
+import DinnerSpinner from './component/DinnerSpinner';
+import PageNotFound from './component/PageNotFound';
+import Elements from './component/elements';
+
+import {
+  HashRouter,
+  Routes, //replaces "Switch" used till v5
+  Route,
+} from "react-router-dom";
+
+import './styles/App.css';
+
 import reportWebVitals from './reportWebVitals';
 
 
@@ -10,8 +27,28 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <App />
+    <div className="App">
+      <Sidebar />
+    <HashRouter>
+        <Routes>
+          <Route path="/index.html" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/resumes" element={<Resumes />} />
+          <Route path="/hobbies" element={<Hobbies />} />
+          <Route path="/dinner-spinner" element={<DinnerSpinner />} />
+          <Route path="/js/about" element={<JsAbout />} />
+          <Route path="/js/syllabus" element={<JsSyllabus />} />
+          
+          <Route path="/elems" element={<Elements />} />
+          {/* <Route path="/js/mod1/lesson1" element={<Home />} /> */}
+          {/* <Route path="/home" element={<Home />} /> */}
+          {/* <Route path="/home" element={<Home />} /> */}
+          {/* <Route path="/home" element={<Home />} /> */}
+          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </HashRouter>
     </div>
   </React.StrictMode>
 );
