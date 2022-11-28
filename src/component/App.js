ee945768b637d6e40f6d51224cf79f2023a68afd
Catherine Sawyer <39068473+crsawyer314@users.noreply.github.com>
2022-11-28 14:38:29 -0700

    added routes for more lessons

--- src/component/App.js
+++ src/component/App.js
@@ -9,6 +9,10 @@ import Hobbies from './Hobbies';
 import DinnerSpinner from './DinnerSpinner';
 import Bio from './Bio';
 import Js1 from './Js1';
+import Js2 from './Js2';
+import Js3 from './Js3';
+import Js4 from './Js4';
+import Js5 from './Js5';
 import PageNotFound from './PageNotFound';
 
 import {
@@ -38,6 +42,10 @@ class App extends Component {
             <Route path="/home" element={<Home />} />
             <Route path="/bio" element={<Bio />} />
             <Route path="/js/1.1" element={<Js1 />} />
+            <Route path="/js/1.2" element={<Js2 />} />
+            <Route path="/js/1.3" element={<Js3 />} />
+            <Route path="/js/1.4" element={<Js4 />} />
+            <Route path="/js/1.5" element={<Js5 />} />
             <Route path="/resumes" element={<Resumes />} />
             <Route path="/hobbies" element={<Hobbies />} />
             <Route path="/dinner-spinner" element={<DinnerSpinner />} />
