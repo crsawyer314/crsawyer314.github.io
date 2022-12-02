0f37cba0c674c2a238ebae8c71a1d5cd10596a7a
Catherine Sawyer <39068473+crsawyer314@users.noreply.github.com>
2022-12-02 09:59:29 -0700

    change js paths

--- src/component/Home.js
+++ src/component/Home.js
@@ -32,7 +32,7 @@ class Home extends Component {
                                                 <p>Technical educator, people manager, software developer, and creator</p>
                                             </header>
                                             <p>My passion is increasing diversity through code.</p>
-                                            <p>Whether it is grade-school students wanting to learn a new hobby or adults looking to break into tech, there is a space for everyone to learn to code. I host some personal projects on this website, including my <a href="/js/about">JavaScript for Adult Beginners</a>, a course targeted at adults looking to upskill and learn code.</p>
+                                            <p>Whether it is grade-school students wanting to learn a new hobby or adults looking to break into tech, there is a space for everyone to learn to code. I host some personal projects on this website, including my <a href="/js_about">JavaScript for Adult Beginners</a>, a course targeted at adults looking to upskill and learn code.</p>
                                             <p>This course is designed for those who have no prior knowledge about code or programming whatsoever, and will guide you not only through your learning journey, but also through the necessary steps to create a meaningful project for your personal portfolio.</p>
                                             <ul className="actions">
                                                 <li><a href="/bio" className="button big">Learn More</a></li>
